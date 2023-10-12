1f9b6244a3911cf1f5ec7e2b2effd7da3e5b729b
Pranay Malhan <pranaymalh@gmail.com>
2023-10-12 19:48:12 +0530

    Added computer guess logic.

--- guess_the_number.py
+++ guess_the_number.py
@@ -11,4 +11,24 @@ def guess(x):
             print("Try again. Your guess is too high.")
     print(f"Yay! you guessed the number {num} correctly.")
 
-guess(100)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    guess = -1
+    while feedback!='c':
+        if low!=high:
+            guess = randint(low,high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high(H), too low(L) or Correct(C) ? ").lower()
+        if feedback=='h':
+            high = guess-1
+        elif feedback=='l':
+            low = guess+1
+    print(f"Yay! The computer guessed your number, {guess}, correctly!")
+
+# Call below function to have computer guess number between 1 to value.
+# computer_guess(100) 
+# Call below function to have computer pick number between 1 to value, which you can guess.
+# guess(100)
