9aa6743e95c3115956a942f31a54b677d260fb0d
Pranay Malhan <pranaymalh@gmail.com>
2023-10-18 17:25:18 +0530

    Updated code

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -1,6 +1,35 @@
-class HumanPlayer:
-    def __init__(self):
+import math
+import random
+
+class Player:
+    def __init__(self,letter):
+        self.letter = letter
+    
+    def get_move(self,game):
         pass
-class ComputerPlayer:
-    def __init__(self):
-        pass
+
+class HumanPlayer(Player):
+    def __init__(self,letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        valid_square = None
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move(0-8):')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid sqaure. Try again.")
+        return val
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
